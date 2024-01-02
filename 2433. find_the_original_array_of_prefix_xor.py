66ad8852a4b752629cdde7916065c2eefa5ddcff
Kai <33795285+kaiicheng@users.noreply.github.com>
2024-01-02 01:47:46 -0500

    Add files via upload

--- 2433. find_the_original_array_of_prefix_xor.py
+++ 2433. find_the_original_array_of_prefix_xor.py
@@ -3,11 +3,22 @@ from typing import List
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
         
-        ans = [0] * len(pref)
-        ans[0] = pref[0]
-        for i in range(1, len(pref)):
-            ans[i] = pref[i] ^ pref[i - 1]
-        return ans
+        # math
+
+        # ans = [0] * len(pref)
+        # ans[0] = pref[0]
+        # for i in range(1, len(pref)):
+        #     ans[i] = pref[i] ^ pref[i - 1]
+        # return ans
+
+
+
+        # math space optimized
+        
+        for i in reversed(range(len(pref))):
+            pref[i] = pref[i] ^ pref[i - 1]
+        return pref
+
 
 def main(pref):
     result = Solution()
