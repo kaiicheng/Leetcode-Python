c545fbd8ccd74bf657851671561f0444fd7452c6
Kai <33795285+kaiicheng@users.noreply.github.com>
2024-01-08 12:06:11 -0500

    Add files via upload

--- 791. custom_sort_string.py
+++ 791. custom_sort_string.py
@@ -4,45 +4,61 @@
 # ls[0].append(2)
 # print("ls: ", ls)
 
+from collections import Counter
+
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         
-        # c = Counter(order)
-        # print("c: ", c)
+        sCounter = Counter(s)
+        res = ""
+        for char in order:
+            if char in sCounter:
+                res += char * sCounter[char]
+                del sCounter[char]
+            print("res: ", res)
+        for char, count in sCounter.items():
+            res += char * count
+        return res
+
+
+
+        # # c = Counter(order)
+        # # print("c: ", c)
         
-        di = {}
-        for i in range(len(order)):
-            di[order[i]] = i
-        # print("di: ", di)
+        # di = {}
+        # for i in range(len(order)):
+        #     di[order[i]] = i
+        # # print("di: ", di)
 
-        # interesting! 
-        # ls = [[]] * len(di)
-        # ls[0].append(1)
-        # ls[0].append(2)
-        # print("ls: ", ls)
+        # # interesting! 
+        # # ls = [[]] * len(di)
+        # # ls[0].append(1)
+        # # ls[0].append(2)
+        # # print("ls: ", ls)
 
-        ls = [[] for i in range(len(order))]
-        # print("ls: ", ls)
+        # ls = [[] for i in range(len(order))]
+        # # print("ls: ", ls)
 
-        res = []
-        for i in range(len(s)):
-            # print("i, s[i]: ", i, s[i])
-            if s[i] in di:
-                # print("di[s[i]]: ", di[s[i]])
-                ls[di[s[i]]].append(s[i])
-            else:
-                res.append(s[i])
-            # print("ls: ", ls)
-        # print("ls: ", ls)
-        # print("res: ", res)
+        # res = []
+        # for i in range(len(s)):
+        #     # print("i, s[i]: ", i, s[i])
+        #     if s[i] in di:
+        #         # print("di[s[i]]: ", di[s[i]])
+        #         ls[di[s[i]]].append(s[i])
+        #     else:
+        #         res.append(s[i])
+        #     # print("ls: ", ls)
+        # # print("ls: ", ls)
+        # # print("res: ", res)
 
-        ans = ls[0]
-        for i in range(1, len(ls)):
-            ans = ans + ls[i]
-        # print("ans: ", ans)
-        ans = ans + res
+        # ans = ls[0]
+        # for i in range(1, len(ls)):
+        #     ans = ans + ls[i]
+        # # print("ans: ", ans)
+        # ans = ans + res
 
-        return "".join(ans)
+        # return "".join(ans)
+
 
 def main(order, s):
     result = Solution()
