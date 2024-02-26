7df90b97cce83c857bc8054543ea4e8fb2dac876
Kai <33795285+kaiicheng@users.noreply.github.com>
2024-02-25 20:27:14 -0500

    Add files via upload

--- 347. top_K_frequent_elements.py
+++ 347. top_K_frequent_elements.py
@@ -1,23 +1,9 @@
-# heap
-
 from typing import List
 from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        # # O(1) time 
-        # if k == len(nums):
-        #     return nums
         
-        # # 1. Build hash map: character and how often it appears
-        # # O(N) time
-        # count = Counter(nums)   
-        # # 2-3. Build heap of top k frequent elements and
-        # # convert it into an output array
-        # # O(N log k) time
-        # return heapq.nlargest(k, count.keys(), key=count.get) 
-
         c = Counter(nums)
         # print("c: ", c)
 
@@ -33,7 +19,7 @@ class Solution:
             ans.append(ls[i][0])
 
         return ans
-    
+
 def main(nums, k):
     result = Solution()
     print(result.topKFrequent(nums, k))
